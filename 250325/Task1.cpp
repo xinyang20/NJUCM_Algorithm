83fef25281a9266436dda9dddf956944b9cef4ae
Xinyang Gao <15850645317@163.com>
2025-03-25 19:41:02 +0800

    第三次作业补充-1
    根据题目要求补充第三次作业第二题的最长子公共子序列

--- 250325/Task1.cpp
+++ 250325/Task1.cpp
@@ -10,15 +10,38 @@ void solve() {
 	int n;
 	cin >> n;
 	std::vector<vector<int> > v(n, vector<int>(n));
+	std::vector<vector<int> > vv(n, vector<int>(n));
+	std::vector<vector<int> > m(n, vector<int>(n, -1));
+	std::vector<int> way(n);
 	for (int row = 0; row < n; row++)
-		for (int col = 0; col <= row; col++)
+		for (int col = 0; col <= row; col++) {
 			cin >> v[row][col];
+			vv[row][col] = v[row][col];
+		}
 
 	for (int row = n - 2; row >= 0; row--)
-		for (int col = 0; col <= row; col++)
-			v[row][col] += min(v[row + 1][col], v[row + 1][col + 1]);
+		for (int col = 0; col <= row; col++) {
+			if (v[row + 1][col] > v[row + 1][col + 1]) {
+				m[row][col] = 1;
+				v[row][col] += v[row + 1][col + 1];
+			}
+			else {
+				m[row][col] = 0;
+				v[row][col] += v[row + 1][col];
+			}
+		}
 
 	cout << v[0][0] << endl;
+
+	int pre_col = 0;
+	for (int i = 0; i < n; i++) {
+		way[n - i - 1] = vv[i][pre_col];
+		pre_col += m[i][pre_col];
+	}
+	for (int i = 0; i < n - 1; i++)
+		cout << way[i] << "->";
+	cout << way[n - 1] << endl;
+
 }
 
 int main() {
